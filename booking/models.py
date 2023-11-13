4e37e5764f7f1c186dd0ea6a839e5b3fa5bf18db
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-11-13 20:13:54 +0000

    Add skeleton of models for admin functionality

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -11,19 +11,23 @@ from cloudinary.models import CloudinaryField
 #     duration_of_stay = check_out - check_in
 #     return duration_of_stay
 
-class Products(models.Model):
+PITCH_CHOICES = ((0, "Tent"), (1, "Caravan"), (2, "Motorhome"), (3, "Van"), (4, "Glamping"))
+CHECK_OUT_TIME = ((11, "11am"), (11, "11am"))
+CHECK_IN_TIME = ((3, "3pm"), (3, "3pm"))
 
-    PITCH_CHOICES = ((0, "Tent"), (1, "Caravan"), (2, "Motorhome"), (3, "Van"), (4, "Glamping"))
-    CHECK_OUT_TIME = ((0, "11am"))
-    CHECK_IN_TIME = ((0, "3pm"))
+class Product(models.Model):
 
     pitch_type = models.IntegerField(choices=PITCH_CHOICES, default=0)
+    pitch_ID = models.CharField(max_length = 6, unique = True)
     check_in_time = models.IntegerField(choices=CHECK_IN_TIME, default=0)
     check_out_time = models.IntegerField(choices=CHECK_OUT_TIME, default=0)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
     date = models.DateField()
     available = models.BooleanField()
     pitch_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.pitch_ID
 
     # def is_available(self):
         # how to check dates are not already booked? 
@@ -31,10 +35,10 @@ class Products(models.Model):
 
 
 class Booking(models.Model):
-    product = models.ForeignKey('Products', on_delete=models.CASCADE, related_name="product_type")
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name="product_type")
     booking_id = models.AutoField(primary_key=True)
-    check_in_date = models.DateTimeField(default=datetime.now)
-    check_out_date = models.DateTimeField(default=datetime.now)
+    check_in_date = models.DateField(default=date.today)
+    check_out_date = models.DateField(default=date.today)
     num_guests = models.IntegerField()
     booking_price = models.FloatField()
 
@@ -48,4 +52,4 @@ class Booking(models.Model):
         return total_price
 
     def __str__(self):
-        return f"Booking made for {self.check_in_date} for {self.duration_of_stay()}"
+        return f"{self.booking_id} for {self.product}"
