e1a038eaa558625e6374e63b3134259477ae6a7d
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2023-11-14 20:33:53 +0000

    add basic html and views

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+# create, view, edit, cancel 
+
+class ProductList(generic.ListView):
+    model = Product
+    queryset = Product.onjects.values_list("pitch_type")
+    template_name = 'booking.html'
+
+
